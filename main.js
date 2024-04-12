2d3e803f028c8bf1259ef78c1919415070bbed9f
Mariana <104834135+marianagomes1234@users.noreply.github.com>
2024-04-12 13:43:43 -0300

    .

--- main.js
+++ main.js
@@ -14,8 +14,9 @@ for (let i = 0; i < botoes.length; i++) {
         textos[i].classList.add("ativo");
     }
 }
+
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+const tempoObjetivo1 = new Date("2024-06-15T23:59:00");
 let tempoAtual = new Date();
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
@@ -23,12 +24,10 @@ contadores[0].textContent = calculaTempo(tempoObjetivo1);
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
-
     let segundos = Math.floor(tempoFinal / 1000);
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24);
-
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
